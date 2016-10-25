0bb43e7e01a76cc2477c0483e3d19d379fd038c4
Iskandar Rafiev <irafiev@gmail.com>
2016-10-25 03:11:03 -0400

    Did the groundwork for the front-end project.

---
+++ src/Loughat/wwwroot/js/appModule.js
@@ -0,0 +1,4 @@
+﻿'use strict';
+
+// Define the `app` module
+angular.module('app', []);
---
+++ src/Loughat/wwwroot/js/componentsTestApp/appComponent.js
@@ -0,0 +1,26 @@
+(function(){
+  'use strict';
+  var app = angular.module('app');
+
+  app.component('menuBar', {
+    // defines a two way binding in and out of the component
+    bindings: {
+      brand:'<'
+     },
+    // Load the template
+    templateUrl: '/js/components/appComponent.html',
+    controller: function () {
+    // A list of menus
+      this.menu = [{
+        name: "Home",
+        component: "home"
+      }, {
+        name: "About",
+        component: "about"
+      }, {
+        name: "Contact",
+        component: "contact"
+      }];
+    }
+  });
+})();
